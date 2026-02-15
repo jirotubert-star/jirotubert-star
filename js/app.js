683d386a7b391f36cd94953181e1bda826cde26e
Elena Taubert <elenataubert@MacBook-Air-von-Elena.local>
2026-02-15 16:42:03 +0100

    Integrate logo and home-screen app icons with manifest, bump to 1.5.22

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ Aufbau der App:
 // LocalStorage Schlüssel
 // ---------------------------
 const STORAGE_KEY = "onestep_state_v1";
-const APP_VERSION = "1.5.21";
+const APP_VERSION = "1.5.22";
 
 // ---------------------------
 // Grundlegende Zeit-Utilities

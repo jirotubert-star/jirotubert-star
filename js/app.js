25caa47fed6eddc2ffccdc4ae97915ad89f86cb1
Elena Taubert <elenataubert@MacBook-Air-von-Elena.local>
2026-02-15 10:56:08 +0100

    Make 100% calendar day border more prominent and bump to 1.5.6

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ Aufbau der App:
 // LocalStorage Schlüssel
 // ---------------------------
 const STORAGE_KEY = "onestep_state_v1";
-const APP_VERSION = "1.5.5";
+const APP_VERSION = "1.5.6";
 
 // ---------------------------
 // Grundlegende Zeit-Utilities
